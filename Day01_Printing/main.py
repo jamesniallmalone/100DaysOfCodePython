e5259a67dbee6444f32db0c990ada71eba781996
James Malone <jamesniallmalone@users.noreply.github.com>
2020-11-30 19:43:19 +0000

    day 1 finished

---
+++ Day01_Printing/main.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator")
+street = input("Where did you grow up?\n")
+pet = input("Whats your pets name?\n")
+print("Your band name might be " + street + " " + pet)
